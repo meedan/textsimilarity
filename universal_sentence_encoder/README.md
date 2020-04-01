cf28600c3308c743228f85343ee399d311c32be5
infojunkie <karim.ratib@gmail.com>
2020-03-31 22:40:53 -0700

    Allow to parameterize model path + more instructions

--- universal_sentence_encoder/README.md
+++ universal_sentence_encoder/README.md
@@ -2,17 +2,17 @@
 
 This is a minimal working example of the Universal Sentence Encoder.
 
-Further detail on the model is available on 
+Further detail on the model is available on
 [TensorFlow Hub](https://tfhub.dev/google/universal-sentence-encoder-large/5)
-and in the original academic paper: 
+and in the original academic paper:
 [Universal Sentence Encoder](https://arxiv.org/pdf/1803.11175.pdf).
 
 `universal_sentence_encoder.py` implements one class, `UniversalSentenceEncoder`.
-The class expects the location (e.g., URL or local filesystem) of a 
+The class expects the location (e.g., URL or local filesystem) of a
 TensorFlow Hub model. It defaults to the URL above.
 
 The class provides two methods:
-* `vectorize` accepts either a string or a list of strings. 
+* `vectorize` accepts either a string or a list of strings.
  If the input is a string, it returns an array with the vector embedding of the string.
  If the input is a list, it returns a matrix with  each row corresponding to one element in the list.
 
@@ -20,5 +20,13 @@ The class provides two methods:
  It is an implementation of Equation 1 in the paper linked above.
 
 ![sim(u,v)=1-\text{arccos}(\frac{u \cdot v}{\left\lVert u\right\rVert \left\lVert v\right\rVert})/\pi](https://render.githubusercontent.com/render/math?math=sim(u%2Cv)%3D1-%5Ctext%7Barccos%7D(%5Cfrac%7Bu%20%5Ccdot%20v%7D%7B%5Cleft%5ClVert%20u%5Cright%5CrVert%20%5Cleft%5ClVert%20v%5Cright%5CrVert%7D)%2F%5Cpi)
- 
 
+## Running
+```
+virtualenv local
+source local/bin/activate
+pip install -r requirements.txt
+python universal_sentence_encoder.py [URL to model file or path to local model folder]
+```
+
+By default, uses data model `universal-sentence-encoder-large` at https://tfhub.dev/google/universal-sentence-encoder-multilingual-large/3. You can also use `universal-sentence-encoder-multilingual-large` at https://tfhub.dev/google/universal-sentence-encoder-multilingual-large/3 or download those locally and point to their extracted directories.
--- universal_sentence_encoder/universal_sentence_encoder.py
+++ universal_sentence_encoder/universal_sentence_encoder.py
@@ -1,12 +1,13 @@
 import tensorflow as tf
 import tensorflow_hub as hub
+import tensorflow_text
 import numpy as np
 import scipy.spatial.distance
 import math
+import sys, getopt
 
 class UniversalSentenceEncoder(object):
-	def __init__(self,model_path="https://tfhub.dev/google/universal-sentence-encoder-large/5"):
-		self.model_path=model_path
+	def __init__(self, model_path):
 		self.model=hub.load(model_path)
 
 	def vectorize(self,doc):
@@ -19,7 +20,7 @@ class UniversalSentenceEncoder(object):
 			return self.model(doc).numpy()
 		else:
 			return self.model([doc]).numpy()[0]
-    
+
 	def angular_simularity(self,u,v):
 		"""
 		Return the angular simularity between two vectors u and v.
@@ -41,25 +42,22 @@ if __name__=="__main__":
 		"How old are you",
 		finalsent,
 	]
-	
-	#This is the mutlilingual model
-	#16 languages (Arabic, Chinese-simplified, Chinese-traditional, English, French, German, Italian, Japanese, Korean, Dutch, Polish, Portuguese, Spanish, Thai, Turkish, Russian)
-	#import tensorflow_text
-	#encoder=UniversalSentenceEncoder(model_path="https://tfhub.dev/google/universal-sentence-encoder-multilingual-large/3")
-	
-	#This is the English-only model
-	encoder=UniversalSentenceEncoder()
-	
+
+	model_path = sys.argv[1] if len(sys.argv) > 1 else "https://tfhub.dev/google/universal-sentence-encoder-large/5"
+	print("Using model %s..." % (model_path))
+
+	encoder=UniversalSentenceEncoder(model_path)
+
 	embeddings=encoder.vectorize(messages) #Check list
 	sent_embedding=encoder.vectorize(finalsent) #Check string
-	
+
 	dists=np.zeros((len(messages),len(messages)))
 	for i in range(0,len(messages)):
 		for j in range(0,len(messages)):
 			dists[i,j]=encoder.angular_simularity(embeddings[i],embeddings[j])
 
 	print(dists)
-	
+
 	#The upper triangle and bottom triangle ought to be the same
 	#because distances are symetric
 	assert np.allclose(dists, dists.T), "Distance matrix is not symmetric"
@@ -68,8 +66,8 @@ if __name__=="__main__":
 	assert dists[0,:].argmax()==1, "The closest sentence to index 0 is not index 1"
 	assert dists[2,:].argmax()==3, "The closest sentence to index 2 is not index 3"
 	assert dists[4,:].argmax()==5, "The closest sentence to index 4 is not index 5"
-	
+
 	assert encoder.angular_simularity(embeddings[5],sent_embedding)==1.0,"Different vectors for same sentence"
-	
+
 	print("All good")
-	
+
