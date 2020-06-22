7ca91fc127a9dfc86b01f5e9b7f844ad424be6ea
ashkank <ashkank@umich.edu>
2020-06-22 13:49:33 -0400

    optimizing claim matching code

--- hindi-sbert/claim_matching_test.py
+++ hindi-sbert/claim_matching_test.py
@@ -44,13 +44,18 @@ def main():
 
     laser_results = []
     sbert_results = []
+    for pair in pairs:
+        pair['lookup_text_laser_embedding'] = get_laser_embedding(pair['lookup_text'])
+        pair['lookup_text_sbert_embedding'] = get_sbert_embedding(pair['lookup_text'])
+        pair['database_text_laser_embedding'] = get_laser_embedding(pair['database_text'])
+        pair['database_text_sbert_embedding'] = get_sbert_embedding(pair['database_text'])
     for threshold in range(1, 101):
-        laser_predictions = [1 if angdist(get_laser_embedding(pair['lookup_text']),
-                                          get_laser_embedding(pair['database_text'])) > threshold / 100 else 0 for pair
-                             in pairs]
-        sbert_predictions = [1 if angdist(get_sbert_embedding(pair['lookup_text']),
-                                          get_sbert_embedding(pair['database_text'])) > threshold / 100 else 0 for pair
-                             in pairs]
+        laser_predictions = [1 if angdist(pair['lookup_text_laser_embedding'],
+                                          pair['database_text_laser_embedding']) > threshold / 100 else 0 for pair in
+                             pairs]
+        sbert_predictions = [1 if angdist(pair['lookup_text_sbert_embedding'],
+                                          pair['database_text_sbert_embedding']) > threshold / 100 else 0 for pair in
+                             pairs]
         tn, fp, fn, tp = confusion_matrix(labels, laser_predictions).ravel()
         laser_results.append([tn, fp, fn, tp])
         tn, fp, fn, tp = confusion_matrix(labels, sbert_predictions).ravel()
@@ -60,3 +65,7 @@ def main():
         print('{},{},{},{},{},{},{},{},{}'.format(i, laser_results[i][0], laser_results[i][1], laser_results[i][2],
                                                   laser_results[i][3], sbert_results[i][0], sbert_results[i][1],
                                                   sbert_results[i][2], sbert_results[i][3]))
+
+
+if __name__ == "__main__":
+    main()
