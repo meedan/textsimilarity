532b50315ad2016723203eb3a8cddd7464ce05e8
ashkank <ashkank@umich.edu>
2020-06-24 17:30:20 -0400

    streaming joining parallel corpuses

--- multilingual-sbert/prepare_parallel_data.py
+++ multilingual-sbert/prepare_parallel_data.py
@@ -1,3 +1,7 @@
+from os import listdir
+from os.path import isfile, join
+
+
 def read_text_file(path):
     with open(path) as fp:
         text = fp.readlines()
@@ -5,23 +9,22 @@ def read_text_file(path):
     return text
 
 
+def get_files_in_directory(mypath):
+    return [f for f in listdir(mypath) if isfile(join(mypath, f))]
+
+
 def join_parallel_text(input_paths, first_language, second_language, output_path):
-    parallel_corpus = []
-
-    for path in input_paths:
-        corpus = {
-            first_language: read_text_file(path[first_language]),
-            second_language: read_text_file(path[second_language])
-        }
-
-        if len(corpus[first_language]) != len(corpus[second_language]):
-            print('error in parallel data: unequal number of rows')
-            return None
-
-        parallel_corpus.append(corpus)
-
     with open(output_path, 'a') as the_file:
-        for corpus in parallel_corpus:
+        for path in input_paths:
+            corpus = {
+                first_language: read_text_file(path[first_language]),
+                second_language: read_text_file(path[second_language])
+            }
+
+            if len(corpus[first_language]) != len(corpus[second_language]):
+                print('error in parallel data: unequal number of rows')
+                return None
+
             corpus_len = len(corpus[first_language])
             for i in range(corpus_len):
                 first_item = corpus[first_language][i]
@@ -83,3 +86,25 @@ def create_english_hindi_parallel_corpus():
     ]
 
     join_parallel_text(test_input_paths, first_language, second_language, 'english_hindi_parallel_test.txt')
+
+
+def create_english_portuguese_parallel_corpus():
+    first_language = 'english'
+    second_language = 'portuguese'
+    portuguese_parallel_corpus_path = '../data/portuguese_parallel_corpus/'
+
+    train_file_names = get_files_in_directory(portuguese_parallel_corpus_path)
+    train_file_names = sorted(train_file_names)
+    train_input_paths = []
+    for i in range(len(train_file_names)//2):
+        train_input_paths.append({
+            first_language: portuguese_parallel_corpus_path + train_file_names[2*i],
+            second_language: portuguese_parallel_corpus_path + train_file_names[2*i + 1]
+        })
+
+    join_parallel_text(train_input_paths, first_language, second_language, 'english_portuguese_parallel_train.txt')
+
+
+if __name__ == "__main__":
+    # create_english_hindi_parallel_corpus()
+    create_english_portuguese_parallel_corpus()
