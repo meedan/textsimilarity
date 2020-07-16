dd0f9ddca09d8b10868676bb60b3a8d4bfd30928
ashkank <ashkank@umich.edu>
2020-07-16 18:26:04 -0400

    adding SBERT training code for training SE asian SBERT

--- multilingual-sbert/student_teacher.py
+++ multilingual-sbert/student_teacher.py
@@ -12,30 +12,30 @@ logging.basicConfig(format='%(asctime)s - %(message)s',
                     handlers=[LoggingHandler()])
 
 max_seq_length = 128
-train_batch_size = 48
+train_batch_size = 64
 
 logging.info("Load teacher model")
 teacher_model = SentenceTransformer('bert-base-nli-stsb-mean-tokens')
 
-# logging.info("Create student model from scratch")
-# word_embedding_model = models.Transformer("xlm-roberta-base")
+logging.info("Create student model from scratch")
+word_embedding_model = models.Transformer("xlm-roberta-base")
 
 # Apply mean pooling to get one fixed sized sentence vector
-# pooling_model = models.Pooling(word_embedding_model.get_word_embedding_dimension(),
-#                                pooling_mode_mean_tokens=True,
-#                                pooling_mode_cls_token=False,
-#                                pooling_mode_max_tokens=False)
+pooling_model = models.Pooling(word_embedding_model.get_word_embedding_dimension(),
+                               pooling_mode_mean_tokens=True,
+                               pooling_mode_cls_token=False,
+                               pooling_mode_max_tokens=False)
 
-# model = SentenceTransformer(modules=[word_embedding_model, pooling_model])
-logging.info("Loading previously trained student-teacher model")
-model = SentenceTransformer('models/hindi-sxlmr-stmodel')
+model = SentenceTransformer(modules=[word_embedding_model, pooling_model])
+# logging.info("Loading previously trained student-teacher model")
+# model = SentenceTransformer('models/hindi-sxlmr-stmodel')
 
-output_path = 'models/multilingual-sbert'
+output_path = 'models/se-asian-sbert'
 
 logging.info("Create dataset reader")
 
 ###### Read Dataset ######
-train_file_path = 'english_portuguese_parallel_train.txt'
+train_file_path = 'train_southeast_asian_parallel_corpus.txt'
 train_data = ParallelSentencesDataset(student_model=model, teacher_model=teacher_model)
 train_data.load_data(train_file_path)
 
@@ -45,12 +45,15 @@ train_loss = losses.MSELoss(model=model)
 ###### Load dev sets ######
 
 # Test on STS 2017.en-de dataset using Spearman rank correlation
-logging.info("Read Portuguese headline-pairs dataset")
+logging.info("Read dev dataset")
 evaluators = []
-claim_pair_reader = ClaimPairDataReader()
-dev_data = SentencesDataset(examples=claim_pair_reader.get_examples(split='train', language='pt'), model=model)
-dev_dataloader = DataLoader(dev_data, shuffle=False, batch_size=train_batch_size)
-evaluator_sts = evaluation.EmbeddingSimilarityEvaluator(dev_dataloader, name='Portuguese Headlines')
+# claim_pair_reader = ClaimPairDataReader()
+# dev_data = SentencesDataset(examples=claim_pair_reader.get_examples(split='train', language='pt'), model=model)
+dev_file_path = 'test_southeast_asian_parallel_corpus.txt'
+dev_data = ParallelSentencesDataset(student_model=model, teacher_model=teacher_model)
+dev_data.load_data(dev_file_path)
+dev_dataloader = DataLoader(dev_data, shuffle=True, batch_size=train_batch_size)
+evaluator_sts = evaluation.EmbeddingSimilarityEvaluator(dev_dataloader, name='SE Asian Test Data')
 evaluators.append(evaluator_sts)
 
 # Train the model
