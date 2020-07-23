5aa05b9901ba54cad6b601b73d225d75bba93f3d
ashkank <ashkank@umich.edu>
2020-07-23 11:21:55 -0400

    removing fuzzy matching from pair annotation selection

--- annotations/select_for_annotation.py
+++ annotations/select_for_annotation.py
@@ -71,41 +71,41 @@ def generate_similarity_matrices():
     # samples = random.sample(samples, 100)
     samples_per_language = group_samples_by_language(samples)
 
-    # for language in samples_per_language:
-    #     # retrieving laser embeddings
-    #     print('retrieving laser embeddings for language: {}'.format(language))
-    #     sample_texts = [item['text'] for item in samples_per_language[language]]
-    #     embeddings = get_laser_embedding(sample_texts, language)
-    #
-    #     # generating laser matrices
-    #     print('calculating laser similarity matrix for language: {}'.format(language))
-    #     similarity_matrix = vcosine(embeddings, embeddings)
-    #     np.fill_diagonal(similarity_matrix, 0)
-    #     np.save('matrices/matrix_laser_{}'.format(language), similarity_matrix)
-    #
-    #     # retrieving sbert embeddings
-    #     print('retrieving sbert embeddings for language: {}'.format(language))
-    #     sbert_model = get_sbert_model(language if language != 'hi-Latn' else 'hi')
-    #     sample_texts = [item['text'] if language != 'hi-Latn' else item['transliterated_text'] for item in samples_per_language[language]]
-    #     embeddings = get_sbert_embedding(sbert_model, sample_texts)
-    #
-    #     # generating sbert matrices
-    #     print('calculating sbert similarity matrix for language: {}'.format(language))
-    #     similarity_matrix = vcosine(embeddings, embeddings)
-    #     np.fill_diagonal(similarity_matrix, 0)
-    #     np.save('matrices/matrix_sbert_{}'.format(language), similarity_matrix)
-
     for language in samples_per_language:
-        sample_size = len(samples_per_language[language])
-        similarity_matrix = np.zeros((sample_size, sample_size))
+        # retrieving laser embeddings
+        print('retrieving laser embeddings for language: {}'.format(language))
+        sample_texts = [item['text'] for item in samples_per_language[language]]
+        embeddings = get_laser_embedding(sample_texts, language)
 
-        print('calculating fuzzy similarity matrix for language: {}'.format(language))
-        for i, sample in enumerate(samples_per_language[language]):
-            for j in range(i+1, len(samples_per_language[language])):
-                other_sample = samples_per_language[language][j]
-                similarity_matrix[i, j] = get_fuzzy_similarity_score(sample['text'], other_sample['text'])
+        # generating laser matrices
+        print('calculating laser similarity matrix for language: {}'.format(language))
+        similarity_matrix = vcosine(embeddings, embeddings)
+        np.fill_diagonal(similarity_matrix, 0)
+        np.save('matrices/matrix_laser_{}'.format(language), similarity_matrix)
 
-        np.save('matrices/matrix_fuzzy_{}'.format(language), similarity_matrix)
+        # retrieving sbert embeddings
+        print('retrieving sbert embeddings for language: {}'.format(language))
+        sbert_model = get_sbert_model(language if language != 'hi-Latn' else 'hi')
+        sample_texts = [item['text'] if language != 'hi-Latn' else item['transliterated_text'] for item in samples_per_language[language]]
+        embeddings = get_sbert_embedding(sbert_model, sample_texts)
+
+        # generating sbert matrices
+        print('calculating sbert similarity matrix for language: {}'.format(language))
+        similarity_matrix = vcosine(embeddings, embeddings)
+        np.fill_diagonal(similarity_matrix, 0)
+        np.save('matrices/matrix_sbert_{}'.format(language), similarity_matrix)
+
+    # for language in samples_per_language:
+    #     sample_size = len(samples_per_language[language])
+    #     similarity_matrix = np.zeros((sample_size, sample_size))
+    #
+    #     print('calculating fuzzy similarity matrix for language: {}'.format(language))
+    #     for i, sample in enumerate(samples_per_language[language]):
+    #         for j in range(i+1, len(samples_per_language[language])):
+    #             other_sample = samples_per_language[language][j]
+    #             similarity_matrix[i, j] = get_fuzzy_similarity_score(sample['text'], other_sample['text'])
+    #
+    #     np.save('matrices/matrix_fuzzy_{}'.format(language), similarity_matrix)
 
 
 def select_pairs_for_annotation():
@@ -116,9 +116,37 @@ def select_pairs_for_annotation():
     for language in samples_per_language:
         laser_matrix = np.load('matrices/matrix_laser_{}.npy'.format(language))
         sbert_matrix = np.load('matrices/matrix_sbert_{}.npy'.format(language))
-        fuzzy_matrix = np.load('matrices/matrix_fuzzy_{}.npy'.format(language))
+        # fuzzy_matrix = np.load('matrices/matrix_fuzzy_{}.npy'.format(language))
 
+        index_pairs_to_annotate = set()
+        _select_indices_within_range(index_pairs_to_annotate, laser_matrix)
+        _select_indices_within_range(index_pairs_to_annotate, sbert_matrix)
+        # _select_indices_within_range(index_pairs_to_annotate, fuzzy_matrix)
+
+        # find disputes between models and add them to annotation list
+        for i in range(len(laser_matrix)):
+            for j in range(i+1, len(laser_matrix[i])):
+                if abs(laser_matrix[i, j] - sbert_matrix[i, j]) >= 0.2:
+                    index_pairs_to_annotate.add((i, j))
+                # elif abs(laser_matrix[i, j] - fuzzy_matrix[i, j]) >= 0.3:
+                #     index_pairs_to_annotate.add((i, j))
+                # elif abs(sbert_matrix[i, j] - fuzzy_matrix[i, j]) >= 0.3:
+                #     index_pairs_to_annotate.add((i, j))
+
+        pairs_to_annotate += [{'item1': samples_per_language[language][i], 'item2': samples_per_language[language][j], 'language': language} for (i, j) in index_pairs_to_annotate]
+
+    with open('pairs_to_annotate.json', 'w') as fp:
+        json.dump(pairs_to_annotate, fp)
+
+
+def _select_indices_within_range(index_pairs_to_annotate, laser_matrix):
+    for i, row in enumerate(laser_matrix):
+        for j in range(i + 1, len(row)):
+            item = row[j]
+            if 0.7 <= item <= 0.9:
+                index_pairs_to_annotate.add((i, j))
 
 
 if __name__ == "__main__":
     generate_similarity_matrices()
+    select_pairs_for_annotation()
